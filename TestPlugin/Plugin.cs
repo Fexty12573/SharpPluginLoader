95342ce069510fdadb702c3b4ad34eb8278e198c
Fexty12573 <bogofogo1@gmail.com>
2023-09-30 22:45:37 +0200

    Small testing plugin

---
+++ TestPlugin/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SharpPluginLoader.Core;
+
+namespace TestPlugin
+{
+    public class Plugin : IPlugin
+    {
+        public PluginData OnLoad()
+        {
+            return new PluginData
+            {
+                OnUpdate = true,
+                IsDebugPlugin = true,
+            };
+        }
+
+        public void OnUpdate(float deltaTime)
+        {
+            Log.Info($"[TestPlugin] OnUpdate: {deltaTime}s");
+        }
+    }
+}

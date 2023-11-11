f1246512ffe2d3ea3ab07481e543f9bdce4fc1d7
Fexty12573 <bogofogo1@gmail.com>
2023-11-11 14:38:46 +0100

    Input class

---
+++ SharpPluginLoader.Core/IO/Button.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpPluginLoader.Core.IO
+{
+    /// <summary>
+    /// The buttons on a controller using Playstation names.
+    /// </summary>
+    [Flags]
+    public enum Button : uint
+    {
+        Share = 1 << 0,
+        L3 = 1 << 1,
+        R3 = 1 << 2,
+        Options = 1 << 3,
+        Up = 1 << 4,
+        Right = 1 << 5,
+        Down = 1 << 6,
+        Left = 1 << 7,
+        L1 = 1 << 8,
+        R1 = 1 << 9,
+        L2 = 1 << 10,
+        R2 = 1 << 11,
+        Triangle = 1 << 12,
+        Circle = 1 << 13,
+        Cross = 1 << 14,
+        Square = 1 << 15,
+        LsUp = 1 << 16,
+        LsRight = 1 << 17,
+        LsDown = 1 << 18,
+        LsLeft = 1 << 19,
+        RsUp = 1 << 20,
+        RsRight = 1 << 21,
+        RsDown = 1 << 22,
+        RsLeft = 1 << 23
+    }
+}
---
+++ SharpPluginLoader.Core/IO/Input.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using SharpPluginLoader.Core.Memory;
+
+namespace SharpPluginLoader.Core.IO
+{
+    /// <summary>
+    /// Provides a set of methods for checking the state of the controller and keyboard.
+    /// </summary>
+    public static class Input
+    {
+        /// <summary>
+        /// Checks if the specified button is currently pressed.
+        /// </summary>
+        public static bool IsDown(Button button) => (PadDown & (uint)button) != 0;
+
+        /// <summary>
+        /// Checks if the specified button was pressed in the last frame.
+        /// </summary>
+        public static bool IsPressed(Button button) => (PadTrg & (uint)button) != 0;
+
+        /// <summary>
+        /// Checks if the specified button was released in the last frame.
+        /// </summary>
+        public static bool IsReleased(Button button) => (PadRel & (uint)button) != 0;
+
+        /// <summary>
+        /// Checks if the specified button was pressed or released in the last frame.
+        /// </summary>
+        public static bool IsChanged(Button button) => (PadChg & (uint)button) != 0;
+
+        /// <summary>
+        /// Checks if the specified button is currently pressed.
+        /// </summary>
+        private static unsafe bool IsDown(VirtualKey key)
+        {
+            var state = KbState;
+            var vk = KbVkTable[(int)key];
+            return (state->On[vk >> 5] & 1u << (vk & 0x1F)) != 0;
+        }
+
+        /// <summary>
+        /// Checks if the specified button was pressed in the last frame.
+        /// </summary>
+        private static unsafe bool IsPressed(VirtualKey key)
+        {
+            var state = KbState;
+            var vk = KbVkTable[(int)key];
+            return (state->Trg[vk >> 5] & 1u << (vk & 0x1F)) != 0;
+        }
+
+        /// <summary>
+        /// Checks if the specified button was released in the last frame.
+        /// </summary>
+        private static unsafe bool IsReleased(VirtualKey key)
+        {
+            var state = KbState;
+            var vk = KbVkTable[(int)key];
+            return (state->Rel[vk >> 5] & 1u << (vk & 0x1F)) != 0;
+        }
+
+        /// <summary>
+        /// Checks if the specified button was pressed or released in the last frame.
+        /// </summary>
+        private static unsafe bool IsChanged(VirtualKey key)
+        {
+            var state = KbState;
+            var vk = KbVkTable[(int)key];
+            return (state->Chg[vk >> 5] & 1u << (vk & 0x1F)) != 0;
+        }
+        
+        
+        private static nint Pad => MemoryUtil.Read<nint>(0x1451c2318);
+        private static nint Keyboard => MemoryUtil.Read<nint>(0x1451c3170);
+
+        private static uint PadDown => (Pad + 0x198).Read<uint>();
+        private static uint PadOld => (Pad + 0x19C).Read<uint>();
+        private static uint PadTrg => (Pad + 0x1A0).Read<uint>();
+        private static uint PadRel => (Pad + 0x1A4).Read<uint>();
+        private static uint PadChg => (Pad + 0x1A8).Read<uint>();
+
+        private static unsafe KeyboardState* KbState => (KeyboardState*)(Keyboard + 0x138);
+        private static unsafe byte* KbVkTable => (byte*)(Keyboard + 0x38);
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    internal unsafe struct KeyboardState
+    {
+        public fixed uint On[8];
+        public fixed uint Old[8];
+        public fixed uint Trg[8];
+        public fixed uint Rel[8];
+        public fixed uint Chg[8];
+        public fixed uint Repeat[8];
+        public fixed ulong RepeatTime[256];
+    }
+}

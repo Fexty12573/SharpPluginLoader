bc993b7394a056bb7109e836df847e4631e06a6d
Fexty12573 <bogofogo1@gmail.com>
2023-11-24 16:12:50 +0100

    NativeArray type

---
+++ SharpPluginLoader.Core/NativeArray.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace SharpPluginLoader.Core
+{
+    public readonly unsafe struct NativeArray<T> : IEnumerable<T> where T : unmanaged
+    {
+        public int Length { get; }
+
+        public nint Pointer { get; }
+
+        public NativeArray(nint pointer, int length)
+        {
+            Pointer = pointer;
+            Length = length;
+        }
+
+        public T this[int index]
+        {
+            get => *(T*)(Pointer + (index * sizeof(T)));
+            set => *(T*)(Pointer + (index * sizeof(T))) = value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (var i = 0; i < Length; i++)
+                yield return this[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}

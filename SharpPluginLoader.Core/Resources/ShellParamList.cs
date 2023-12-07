9a6e321362142707727e95b148ddfdd9dadd0743
Fexty12573 <bogofogo1@gmail.com>
2023-12-07 19:27:38 +0100

    Small shll addition

--- SharpPluginLoader.Core/Resources/ShellParamList.cs
+++ SharpPluginLoader.Core/Resources/ShellParamList.cs
@@ -11,6 +11,36 @@ namespace SharpPluginLoader.Core.Resources
         public ShellParamList() { }
 
         /// <summary>
+        /// Gets the number of shells in this list.
+        /// </summary>
+        public int ShellCount => Get<int>(0xB0);
+
+        /// <summary>
+        /// Gets all shells in this list.
+        /// </summary>
+        public ShellParam[] Shells
+        {
+            get
+            {
+                var count = ShellCount;
+                if (count == 0)
+                    return [];
+
+                var begin = Get<nint>(0xA8);
+                var end = begin + count * 0x10;
+                var shells = new ShellParam[count];
+                for (var i = 0; begin < end; begin += 0x10)
+                {
+                    var shell = begin.Read<nint>(0x8);
+                    if (shell != 0)
+                        shells[i++] = new ShellParam(shell);
+                }
+
+                return shells;
+            }
+        }
+
+        /// <summary>
         /// Gets a shell by its index.
         /// </summary>
         /// <param name="index"></param>
@@ -19,7 +49,7 @@ namespace SharpPluginLoader.Core.Resources
         {
             // GetShellFunc is a native function that returns a pointer to a cShellParamList object.
             // The actual shell is stored at offset 8 from this pointer.
-            var shell = GetShellFunc.Invoke(Instance, index);
+            var shell = GetShellFunc.InvokeUnsafe(Instance, index);
             if (shell == 0)
                 return null;
 

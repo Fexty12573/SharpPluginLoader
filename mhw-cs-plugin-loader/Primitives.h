824d448f399c5a1b57d169a6b85454e6ae5e09e1
Fexty12573 <bogofogo1@gmail.com>
2024-02-03 11:40:11 +0100

    New line primitive

--- mhw-cs-plugin-loader/Primitives.h
+++ mhw-cs-plugin-loader/Primitives.h
@@ -8,7 +8,7 @@ dti_size_assert(MtSphere, 0x10);
 struct Sphere {
     MtSphere sphere;
     MtColorF color;
-    Sphere(const MtSphere& sphere, MtColorF color) : sphere(sphere), color(color) {}
+    Sphere(const MtSphere& sphere, const MtColorF& color) : sphere(sphere), color(color) {}
 };
 
 dti_offset_assert(MtCapsule, p1, 0x10);
@@ -16,7 +16,7 @@ dti_offset_assert(MtCapsule, r, 0x20);
 struct Capsule {
     MtCapsule capsule;
     MtColorF color;
-    Capsule(const MtCapsule& capsule, MtColorF color) : capsule(capsule), color(color) {}
+    Capsule(const MtCapsule& capsule, const MtColorF& color) : capsule(capsule), color(color) {}
 };
 
 dti_offset_assert(MtOBB, extent, 0x40);
@@ -24,7 +24,15 @@ dti_size_assert(MtOBB, 0x50);
 struct OBB {
     MtOBB obb;
     MtColorF color;
-    OBB(const MtOBB& obb, MtColorF color) : obb(obb), color(color) {}
+    OBB(const MtOBB& obb, const MtColorF& color) : obb(obb), color(color) {}
+};
+
+dti_offset_assert(MtLineSegment, p1, 0x10);
+dti_size_assert(MtLineSegment, 0x20);
+struct Line {
+    MtLineSegment line;
+    MtColorF color;
+    Line(const MtLineSegment& line, const MtColorF& color) : line(line), color(color) {}
 };
 
 }

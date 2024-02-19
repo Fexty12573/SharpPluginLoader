9388187ed3944a2cfbfdb1b3613ebb511c8aa2c4
Fexty12573 <bogofogo1@gmail.com>
2024-02-19 20:07:36 +0100

    Example usage for textures

--- Examples/ExperimentalTesting/Plugin.cs
+++ Examples/ExperimentalTesting/Plugin.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Specialized;
+using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -24,6 +25,15 @@ namespace ExperimentalTesting
         private string _str = "Hello World!";
         private long _a, _b, _sum;
 
+        private TextureHandle _texture = TextureHandle.Invalid;
+        private uint _textureWidth, _textureHeight;
+        private bool _firstRender = true;
+        private string _texturePath = @"em\em001\00\mod\em001_00_BML";
+        private MtDti _texDti = null!;
+        private bool _showRealSize = false;
+
+        private (Texture texture, TextureHandle handle) _loadedTexture = (null!, TextureHandle.Invalid);
+
         public unsafe void CreateShinyDropHook(MtObject a, int b, int c, nint d, long e, uint f)
         {
             Log.Info($"CreateShinyDropHook: {a},{b},{c},{d},{e},{f}");
@@ -47,12 +57,26 @@ namespace ExperimentalTesting
 
         public void OnLoad()
         {
+            _texDti = MtDti.Find("rTexture")!;
+            Ensure.NotNull(_texDti);
+
             //_createShinyDropHook = MarshallingHook.Create<CreateShinyDropDelegate>(CreateShinyDropHook, 0x1402cb1d0);
             //_releaseResourceHook = MarshallingHook.Create<ReleaseResourceDelegate>(ReleaseResourceHook, 0x142224890);
         }
 
         public void OnImGuiRender()
         {
+            if (_firstRender)
+            {
+                _texture = Renderer.LoadTexture("./yodabruh.png", out _textureWidth, out _textureHeight);
+                _firstRender = false;
+
+                if (_texture == TextureHandle.Invalid)
+                    Log.Error("Failed to load texture!");
+                else
+                    Log.Info("Texture loaded!");
+            }
+
             ImGui.InputText("Message", ref _str, 512);
             ImGui.SameLine();
             if (ImGui.Button("Display Fatal Error"))
@@ -77,6 +101,38 @@ namespace ExperimentalTesting
             ImGui.SameLine();
             if (ImGui.Button("Modify"))
                 InternalCalls.ModifyValue(ref _value);
+
+            if (_texture != TextureHandle.Invalid)
+            {
+                ImGui.Image(_texture, new Vector2(_textureWidth, _textureHeight));
+            }
+
+            ImGui.Separator();
+
+            ImGui.InputText("Texture Path", ref _texturePath, 260);
+            ImGui.SameLine();
+            if (ImGui.Button("Load Texture"))
+            {
+                var tex = ResourceManager.GetResource<Texture>(_texturePath, _texDti);
+                if (tex is null)
+                {
+                    Log.Error("Failed to load texture!");
+                    return;
+                }
+
+                _loadedTexture = (tex, tex.GetTextureHandle());
+            }
+
+            ImGui.SameLine();
+            ImGui.Checkbox("Real Size", ref _showRealSize);
+
+            if (_loadedTexture.handle != TextureHandle.Invalid)
+            {
+                ImGui.Image(_loadedTexture.handle, _showRealSize
+                    ? new Vector2(_loadedTexture.texture.Width, _loadedTexture.texture.Height)
+                    : new Vector2(256, 256)
+                );
+            }
         }
     }
 }

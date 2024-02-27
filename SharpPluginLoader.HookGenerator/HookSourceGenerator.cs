017df84719e5e2c44498b81ade6fac1952d4f226
Fexty <bogofogo1@gmail.com>
2024-02-27 13:30:45 +0100

    HookGenerator project

---
+++ SharpPluginLoader.HookGenerator/HookSourceGenerator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace SharpPluginLoader.HookGenerator;
+
+[Generator]
+public class HookSourceGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+}

1b0ed9375aeb81abacc325cc28c1d69c902d7188
Fexty <bogofogo1@gmail.com>
2024-04-23 15:29:54 +0200

    IAllocator interfaces to simplify native memory allocation

---
+++ SharpPluginLoader.Core/Memory/IAllocator.cs
@@ -0,0 +1,28 @@
+﻿namespace SharpPluginLoader.Core.Memory;
+
+/// <summary>
+/// Describes an allocator that can allocate and free unmanaged memory.
+/// </summary>
+public unsafe interface IAllocator
+{
+    /// <summary>
+    /// Allocates a block of memory of the specified size.
+    /// </summary>
+    /// <param name="size">The size of the memory block to allocate.</param>
+    /// <returns>A pointer to the allocated memory block.</returns>
+    public void* Allocate(nint size);
+
+    /// <summary>
+    /// Allocates <paramref name="count"/> instances of <typeparamref name="TObj"/>.
+    /// </summary>
+    /// <typeparam name="TObj">The type of object to allocate.</typeparam>
+    /// <param name="count">The number of instances to allocate.</param>
+    /// <returns>A pointer to the allocated memory block.</returns>
+    public TObj* Allocate<TObj>(int count) where TObj : unmanaged;
+
+    /// <summary>
+    /// Frees a previously allocated memory block.
+    /// </summary>
+    /// <param name="ptr">The pointer to the memory block to free.</param>
+    public void Free(void* ptr);
+}
---
+++ SharpPluginLoader.Core/Memory/IResizableAllocator.cs
@@ -0,0 +1,24 @@
+﻿namespace SharpPluginLoader.Core.Memory;
+
+/// <summary>
+/// Describes an allocator that can also reallocate memory blocks.
+/// </summary>
+public unsafe interface IResizableAllocator : IAllocator
+{
+    /// <summary>
+    /// Reallocates a previously allocated memory block to the specified size.
+    /// </summary>
+    /// <param name="ptr">The pointer to the memory block to reallocate.</param>
+    /// <param name="size">The new size of the memory block.</param>
+    /// <returns>A pointer to the reallocated memory block.</returns>
+    public void* Reallocate(void* ptr, nint size);
+
+    /// <summary>
+    /// Reallocates a previously allocated object array to the specified number of instances.
+    /// </summary>
+    /// <typeparam name="TObj">The type of object to reallocate.</typeparam>
+    /// <param name="ptr">The pointer to the memory block to reallocate.</param>
+    /// <param name="count">The new number of instances to allocate.</param>
+    /// <returns>A pointer to the reallocated memory block.</returns>
+    public TObj* Reallocate<TObj>(TObj* ptr, int count) where TObj : unmanaged;
+}

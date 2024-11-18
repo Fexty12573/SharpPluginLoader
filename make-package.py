8bfd4d6759bec03e1120d28078d8356750e9fadf
Fexty12573 <bogofogo1@gmail.com>
2024-11-18 11:09:22 +0100

    Generate separate windows and linux packages

--- make-package.py
+++ make-package.py
@@ -5,8 +5,7 @@ import os
 
 def main(sln_dir, config, tag):
     # build the solution
-
-    print(f"Building solution in {sln_dir} with configuration {config}...")
+    print(f'Building solution in {sln_dir} with configuration {config}...')
     
     msbuild = f"{os.environ['ProgramFiles']}\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe"
     sln = os.path.join(sln_dir, "mhw-cs-plugin-loader.sln")
@@ -14,8 +13,9 @@ def main(sln_dir, config, tag):
 
     # create the release directory
     release_dir = os.path.join(sln_dir, "release-files")
-    root_dir = os.path.join(release_dir, config)
-    plugins_dir = os.path.join(root_dir, "nativePC/plugins")
+    root_dir_win32 = os.path.join(release_dir, config + "-win32")
+    root_dir_linux = os.path.join(release_dir, config + "-linux")
+    plugins_dir = os.path.join(root_dir_win32, "nativePC/plugins")
     loader_dir = os.path.join(plugins_dir, "CSharp/Loader")
     tag_dir = os.path.join(release_dir, "tags", tag)
 
@@ -34,24 +34,39 @@ def main(sln_dir, config, tag):
     chunk_dst = os.path.join(loader_dir, chunk_name)
 
     native_src = os.path.join(sln_dir, "x64", config, "mhw-cs-plugin-loader.dll")
-    native_dst = os.path.join(root_dir, "winmm.dll")
+    native_winmm_dst = os.path.join(root_dir_win32, "winmm.dll")
+    native_msvcrt_dst = os.path.join(root_dir_linux, "msvcrt.dll")
 
     runtimeconfig_src = os.path.join(sln_dir, "mhw-cs-plugin-loader/SharpPluginLoader.runtimeconfig.json")
     runtimeconfig_dst = os.path.join(loader_dir, "SharpPluginLoader.runtimeconfig.json")
 
-    # copy the files
+    # copy the files for windows (winmm.dll)
     shutil.copyfile(boostrapper_src, boostrapper_dst)
     shutil.copyfile(core_src, core_dst)
     shutil.copyfile(chunk_src, chunk_dst)
-    shutil.copyfile(native_src, native_dst)
+    shutil.copyfile(native_src, native_winmm_dst)
     shutil.copyfile(runtimeconfig_src, runtimeconfig_dst)
 
-    # create the zip file
-    zip_name = f"SharpPluginLoader-{tag}-{config}" if config == "Debug" else f"SharpPluginLoader-{tag}"
-    zip_path = os.path.join(tag_dir, zip_name)
-    shutil.make_archive(zip_path, 'zip', root_dir)
+    # copy the files for linux (msvcrt.dll)
+    shutil.copytree(root_dir_win32, root_dir_linux, dirs_exist_ok=True)
+    shutil.copyfile(native_src, native_msvcrt_dst)
+    os.remove(os.path.join(root_dir_linux, "winmm.dll"))
 
-    print(f"Created release: {zip_path}.zip")
+    # create the zip file
+    zip_name_win32 = f"SharpPluginLoader-{tag}-{config}-win32" if config == "Debug" else f"SharpPluginLoader-{tag}-win32"
+    zip_name_linux = f"SharpPluginLoader-{tag}-{config}-linux" if config == "Debug" else f"SharpPluginLoader-{tag}-linux"
+    zip_path_win32 = os.path.join(tag_dir, zip_name_win32)
+    zip_path_linux = os.path.join(tag_dir, zip_name_linux)
+
+    shutil.make_archive(zip_path_win32, 'zip', root_dir_win32)
+    shutil.make_archive(zip_path_linux, 'zip', root_dir_linux)
+
+    print(f"Created release: {zip_path_win32}.zip")
+    print(f"Created release: {zip_path_linux}.zip")
+
+def usage(parser: argparse.ArgumentParser):
+    parser.print_help()
+    exit(1)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Create a release of the plugin loader")
@@ -60,4 +75,8 @@ if __name__ == "__main__":
     parser.add_argument("tag", help="The tag to use for the release in the format x.x.x[.x]", default="latest")
     args = parser.parse_args()
 
+    if not os.path.isdir(args.sln_dir):
+        print(f"Error: {args.sln_dir} is not a directory")
+        usage(parser)
+    
     main(args.sln_dir, args.config, args.tag)

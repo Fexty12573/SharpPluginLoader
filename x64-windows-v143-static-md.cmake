afb320612a041630d6f15857d39ff9cf9c656bf6
Andrew Opalach <andrew@akon.city>
2026-05-21 16:17:16 -0400

    Explicitly use Platform Toolset v143 with vcpkg

---
+++ x64-windows-v143-static-md.cmake
@@ -0,0 +1,5 @@
+# https://github.com/microsoft/vcpkg/blob/master/triplets/x64-windows-static-md.cmake
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_PLATFORM_TOOLSET "v143")

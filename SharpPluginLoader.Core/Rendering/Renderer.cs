507bbc1dc4f70d8a6db2be4dab4dd8d3ada86baa
Andrew Opalach <andrew@akon.city>
2026-05-22 22:01:48 -0400

    Add shader replacement functionality

--- SharpPluginLoader.Core/Rendering/Renderer.cs
+++ SharpPluginLoader.Core/Rendering/Renderer.cs
@@ -466,6 +466,13 @@ namespace SharpPluginLoader.Core.Rendering
             return (nint)ImGui.GetDrawData().NativePtr;
         }
 
+        [UnmanagedCallersOnly]
+        internal static unsafe void CreateShader(ShaderInfo* info)
+        {
+            foreach (var plugin in PluginManager.Instance.GetPlugins(p => p.OnCreateShader))
+                plugin.OnCreateShader(info);
+        }
+
         internal static void Shutdown()
         {
             ImGui.DestroyContext();
